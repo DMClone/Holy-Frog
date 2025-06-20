20ba0bdebfe59572331556963251fbc54ddeb8ed
DMClone <230381@student.glu.nl>
2025-06-20 11:46:06 +0200

    Ant throwing begin

--- Assets/Scripts/Enemies/Ant.cs
+++ Assets/Scripts/Enemies/Ant.cs
@@ -1,12 +1,20 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Ant : Mite
 {
     [Header("Settings")]
+    [SerializeField] Log[] _logs;
+    [SerializeField] private int _throwStacks;
+    [SerializeField] private int _maxThrowStacks = 20; // Number of stacks before throwing a log
+    [SerializeField] private float _throwInterval = 2f; // Time between holding log and throwing it
+    [SerializeField][ReadOnly] private int _currentLog;
+    [SerializeField][ReadOnly] private bool _holdingLog;
     [Tooltip("Distance before needing to stop")][SerializeField][Range(0, 50)] private int _stopDistance;
     [Tooltip("Distance before needing to back up")][SerializeField][Range(0, 50)] private int _backupDistance;
     public float _distance;
+
     protected override void FixedUpdate()
     {
         float distance = GetDistanceToPlayer();
@@ -17,19 +25,50 @@ public class Ant : Mite
             Vector3 fromPlayer = _playerTransform.position - transform.position;
             _navMeshAgent.SetDestination(transform.position - fromPlayer * 2);
             _navMeshAgent.angularSpeed = 0;
+            _throwStacks = 0;
         }
 
         else if (distance > _stopDistance)
         {
             _navMeshAgent.angularSpeed = 360;
             base.FixedUpdate();
+            _throwStacks++;
 
         }
         else if (_navMeshAgent.hasPath)
         {
             _navMeshAgent.ResetPath();
             Debug.Log("Stopped");
+            _throwStacks++;
         }
 
+        if (_throwStacks < _maxThrowStacks)
+        {
+            StartCoroutine(HoldLog());
+        }
+    }
+
+    private IEnumerator HoldLog()
+    {
+        _holdingLog = true;
+        yield return new WaitForSeconds(_throwInterval);
+        ThrowLog();
+        _holdingLog = false;
+    }
+
+    private void ThrowLog()
+    {
+        if (_currentLog >= _logs.Length)
+        {
+            _currentLog = 0;
+        }
+
+        Log log = _logs[_currentLog];
+        log.gameObject.SetActive(true);
+        log.transform.position = transform.position + Vector3.up * 0.5f; // Adjust height if needed
+        log.transform.rotation = Quaternion.LookRotation(_playerTransform.position - transform.position);
+        log._rigidbody.linearVelocity = (log.transform.forward * 10f) + Vector3.up * 5f; // Adjust speed and angle as needed
+
+        _currentLog++;
     }
 }
--- Assets/Scripts/Enemies/Log.cs
+++ Assets/Scripts/Enemies/Log.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class Log : MonoBehaviour
 {
+    [Header("Dependencies")]
+    public Rigidbody _rigidbody;
     private GameManager _gameManager;
-    [SerializeField] private Rigidbody _rigidbody;
+    [Header("Settings")]
+    [SerializeField] private float _minScale = 0.1f;
+    [Tooltip("Time to reach min scale in seconds")][SerializeField] private float _scaleSpeed = 4f;
+    private Vector3 _startPos;
+    private Vector3 _lastVelocity;
 
     void Start()
     {
         _gameManager = GameManager.instance;
         _gameManager.ue_sceneReset.AddListener(OnReset);
+        _startPos = transform.position;
     }
 
     public void OnReset()
@@ -16,7 +23,29 @@ public class Log : MonoBehaviour
         if (!gameObject.activeSelf)
             gameObject.SetActive(true);
 
-        transform.position = new Vector3(0, 0, 0); // Reset position to origin or any desired position
+        transform.position = new Vector3(0, 0, 0);
+        transform.localScale = Vector3.one;
+        _rigidbody.linearVelocity = Vector3.zero;
+    }
 
+    private void FixedUpdate()
+    {
+        _lastVelocity = _rigidbody.linearVelocity;
+
+        if (transform.localScale.x > 0.1f)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, _scaleSpeed * Time.fixedDeltaTime);
+            if (transform.localScale.x < _minScale)
+            {
+                transform.localScale = new Vector3(_minScale, _minScale, _minScale);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _rigidbody.linearVelocity = _lastVelocity;
+        _rigidbody.linearVelocity += Vector3.up * 5f;
     }
 }
